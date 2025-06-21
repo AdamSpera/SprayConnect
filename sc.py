68e0aaa2db2c8ec3815fe141646205f47557f073
adamspera <adamspera@hotmail.com>
2025-06-21 04:43:49 -0400

    Wipe

--- sc.py
+++
@@ -1,5 +0,0 @@
-from sc.cli import main
-
-if __name__ == "__main__":
-    import sys
-    sys.exit(main())
--- sc/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Spray Connect package."""
-__all__ = ["cli", "connection_manager", "logger"]
